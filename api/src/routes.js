9d7be70e3bbb39c754994752970b98d55d508fc5
richardpns <richardpinesi022@gmail.com>
2024-04-04 08:23:13 -0300

    projeto

---
+++ api/src/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = express.Router();
+
+const Usuario = require('./controllers/usuario');
+const Tarefa = require('./controllers/tarefa');
+
+routes.get('/', (req, res) => {
+    res.json("API Tarefas 1.0")
+});
+
+routes.post('/usuarios/login', Usuario.login);
+routes.post('/usuarios', Usuario.addUsuario);
+routes.get('/usuarios', Usuario.getUsuarios);
+routes.get('/usuarios/:id', Usuario.getUsuarios);
+routes.put('/usuarios', Usuario.updateUsuario);
+routes.delete('/usuarios/:id', Usuario.deleteUsuario);
+
+routes.post('/tarefas', Tarefa.addTarefa);
+routes.get('/tarefas', Tarefa.getTarefas);
+routes.get('/tarefas/:id', Tarefa.getTarefas);
+routes.get('/tarefas/status/:status', Tarefa.getTarefaStatus);
+routes.get('/tarefas/data/:data', Tarefa.getTarefaData);    
+routes.put('/tarefas', Tarefa.updateTarefa);
+routes.delete('/tarefas/:id', Tarefa.deleteTarefa);
+
+module.exports = routes;
